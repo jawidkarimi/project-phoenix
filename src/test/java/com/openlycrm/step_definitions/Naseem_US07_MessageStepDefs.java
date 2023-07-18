9f387fae980c5d84f3304ee6290885a5692655d1
naseemalshareef <alshareefnaseem86@gmail.com>
2023-07-17 22:29:49 -0700

    Create My Feature,StepD and Page updates

--- src/test/java/com/openlycrm/step_definitions/Naseem_US07_MessageStepDefs.java
+++ src/test/java/com/openlycrm/step_definitions/Naseem_US07_MessageStepDefs.java
@@ -1,4 +1,64 @@
 package com.openlycrm.step_definitions;
 
+import com.openlycrm.pages.LoginPage;
+import com.openlycrm.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.By;
+
 public class Naseem_US07_MessageStepDefs {
+
+    LoginPage loginPage = new LoginPage();
+
+    @Given("user Verify seen Employees button after LogIn")
+    public void userVerifySeenEmployeesButtonAfterLogIn() {
+    Driver.getDriver().findElement(By.xpath("(//a[@class='menu-item-link'])[10]"));
+
+
+    }
+    @Then("user Verify the  view the following {eight} modules in the Employees page.")
+    public void userVerifyTheViewTheFollowingModulesInTheEmployeesPage(int eight) {
+
+
+        Driver.getDriver().findElement(By.xpath("//div[@class='main-buttons-inner-container']"));
+
+    }
+
+    @Then("User can see Company Structure")
+    public void user_can_see_company_structure() {
+
+    }
+
+    @Then("User can see Telephone Directory")
+    public void user_can_see_telephone_directory() {
+
+    }
+    @Then("User sees Staff Changes")
+    public void user_sees_staff_changes() {
+
+    }
+    @Then("User sees Efficiency Report")
+    public void user_sees_efficiency_report() {
+
+    }
+    @Then("User sees Honored Employees")
+    public void user_sees_honored_employees() {
+
+    }
+    @Then("User sees Birthdays")
+    public void user_sees_birthdays() {
+
+    }
+    @Then("User sees New page")
+    public void user_sees_new_page() {
+
+    }
+
+
+    @Then("User sees Find Employee")
+    public void userSeesFindEmployee() {
+    }
+
+
+
 }
--- src/test/resources/features/Naseem_US07_Employees.feature
+++ src/test/resources/features/Naseem_US07_Employees.feature
@@ -6,13 +6,14 @@ Feature:Access Employees Page
   Background: User is already in the log in page
     Given the user is on the login page
 
-  Scenario:
-    Given Verify the users view the following 8 modules in the Employees page.
-    Then Company Structure
-    Then Find Employee
-    Then Telephone Directory
-    And Staff Changes
-    And Efficiency Report
-    Then Honored Employees
-    And Birthdays
-    Then New page
+  Scenario:Access Employees Page
+    Given user Verify seen Employees button after LogIn
+   Then user Verify the  view the following 8 modules in the Employees page.
+    Then  User can see Company Structure
+    Then User sees Find Employee
+    Then User can see Telephone Directory
+    And User sees Staff Changes
+    And User sees Efficiency Report
+    Then User sees Honored Employees
+    And User sees Birthdays
+    Then User sees New page
