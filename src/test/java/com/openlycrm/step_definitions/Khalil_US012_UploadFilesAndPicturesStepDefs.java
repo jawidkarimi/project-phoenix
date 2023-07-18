03043be8bff2fae34430cbaaf220c812a1be9ff7
khali <khalil.elkhoja@hotmail.co.uk>
2023-07-17 19:51:14 -0700

    Added scenarios to feature and snippets to stepdef.

---
+++ src/test/java/com/openlycrm/step_definitions/Khalil_US012_UploadFilesAndPicturesStepDefs.java
@@ -0,0 +1,50 @@
+package com.openlycrm.step_definitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Khalil_US012_UploadFilesAndPicturesStepDefs {
+
+    @When("user click on MORE")
+    public void user_click_on_more() {
+
+    }
+    @When("user click on Appreciation")
+    public void user_click_on_appreciation() {
+
+    }
+    @When("user click on upload files icon")
+    public void user_click_on_upload_files_icon() {
+
+    }
+    @When("user click on upload files and images icon")
+    public void user_click_on_upload_files_and_images_icon() {
+
+    }
+    @When("user upload the file or image")
+    public void user_upload_the_file_or_image() {
+
+    }
+    @When("user click on Insert in text icon")
+    public void user_click_on_insert_in_text_icon() {
+
+    }
+    @When("user click on the X icon")
+    public void user_click_on_the_x_icon() {
+
+    }
+    @Then("user should not see file or image in the text box")
+    public void user_should_not_see_file_or_image_in_the_text_box() {
+
+    }
+    @Then("user should see uploaded file or image under the text box")
+    public void user_should_see_uploaded_file_or_image_under_the_text_box() {
+
+    }
+    @Then("user should see file or image in the text box")
+    public void user_should_see_file_or_image_in_the_text_box() {
+
+    }
+
+
+}
