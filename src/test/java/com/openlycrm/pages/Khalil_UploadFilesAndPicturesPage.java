70e4aabaa43709534e3155a587e8a10319a1cbd7
khali <khalil.elkhoja@hotmail.co.uk>
2023-07-18 18:08:26 -0700

    Added my Page

---
+++ src/test/java/com/openlycrm/pages/Khalil_UploadFilesAndPicturesPage.java
@@ -0,0 +1,14 @@
+package com.openlycrm.pages;
+
+import com.openlycrm.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class Khalil_UploadFilesAndPicturesPage {
+
+    public Khalil_UploadFilesAndPicturesPage(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+
+    }
+
+}
