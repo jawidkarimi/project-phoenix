e0fb2a8cfb737c6fe677e52718438ebf481f8546
jawidkarimi <104651569+jawidkarimi@users.noreply.github.com>
2023-07-20 16:56:32 -0700

    login update

--- src/test/java/com/openlycrm/step_definitions/Hooks.java
+++ src/test/java/com/openlycrm/step_definitions/Hooks.java
@@ -24,7 +24,7 @@ public class Hooks {
     @Before (order = 1)
     public void setupMethod(){
 
-        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
         Driver.getDriver().get(ConfigurationReader.getProperty("url"));
     }
@@ -50,7 +50,7 @@ public class Hooks {
 
 
         BrowserUtils.sleep(2);
-        //Driver.closeDriver();
+        Driver.closeDriver();
 
     }
 
