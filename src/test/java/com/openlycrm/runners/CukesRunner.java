da40c657d4cbed0b2c4e4d31e5f9819ad0de2cff
MFadli603 <elfadili.med4@gmail.com>
2023-07-20 19:22:37 -0700

    cleaned my code

--- src/test/java/com/openlycrm/runners/CukesRunner.java
+++ src/test/java/com/openlycrm/runners/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         glue = "com/openlycrm/step_definitions",
         dryRun = false,
   
-        tags = "@Khalil",
+        tags = "",
 
         publish = true //generating a report with public link
 )
