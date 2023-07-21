a7fe3ff0927d3e4f2936bd27ab2f08d2452d8c4b
khali <khalil.elkhoja@hotmail.co.uk>
2023-07-20 17:22:19 -0700

    WIP

--- src/test/java/com/openlycrm/runners/CukesRunner.java
+++ src/test/java/com/openlycrm/runners/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         glue = "com/openlycrm/step_definitions",
         dryRun = false,
   
-        tags = "@login",
+        tags = "@Khalil",
 
         publish = true //generating a report with public link
 )
